f4df2c69ea514ce6910f746969b08f1530e52c1a
unknown <alghenriyb@gmail.com>
2022-08-31 01:19:10 +0200

    formulaire de contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,10 +3,17 @@ import './contact.scss';
 function Contact() {
   return (
     <main className="contact-container">
-      <h1>Contact</h1>
-      {/* <div className="contact-content">
-        
-      </div> */}
+      <h1 className="contact-title">Contact</h1>
+      <div className="contact-content">
+        <form className="contact-form">
+          <input type="text" name="nom" value="" placeholder="Nom" className="contact-input" required></input>
+          <input type="text" name="prenom" value="" placeholder="Prenom" className="contact-input" required></input>
+          <input type="email" name="mail" value="" placeholder="Mail" className="contact-input" required></input>
+          <input type="text" name="sujet" value="" placeholder="Sujet" className="contact-input" required></input>
+          <textarea name="message" value="" placeholder="Message" className="contact-input" required></textarea>
+          <button type="submit" name="envoyer" className="contact-input" id="contact-button">Envoyer</button>
+        </form>
+      </div>
     </main>
   );
 }
